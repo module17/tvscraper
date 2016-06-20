a945d148fc95ad6c3f91357dde8756b5333b4bfe
Darek Kowalski <module17@gmail.com>
2016-06-20 00:45:45 -0400

    Funnel

--- src/Tvscraper.php
+++ src/Tvscraper.php
@@ -22,7 +22,12 @@ class Tvscraper {
     public $schedule_size = 4;
     public $base_url = 'http://tvpassport.com/';
     public $schedule_url = 'tvgrid.shtml?';
+    public $provider_search_url = 'provider_search.shtml';
     public $times = array();
+    public $service_areas = array();
+    public $provider_data = array();
+    public $postal_code = '';
+    public $city_code = '';
 
 
     /*
@@ -34,16 +39,134 @@ class Tvscraper {
             $this->debug = $config['debug'];
             $this->display_single = $config['display_single'];
         }
+        $this->outputBanner();
         $this->readConfig();
+    }
+
+    public function outputBanner() {
+        echo <<<DATA
+********************************************************************************
+******************************* tvscraper **************************************
+********************************************************************************
+
+DATA;
     }
 
     public function readConfig() {
         try {
             $config = Yaml::parse(file_get_contents(__DIR__ . '/../config.yml'));
         } catch (ParseException $e) {
-            throw new \Exception(sprintf("Unable to parse the YAML config file: %s", $e->getMessage()));
+            throw new \Exception(sprintf('Unable to parse the YAML config file: %s', $e->getMessage()));
         }
         $this->initProvider($config['listing_id'], $config['timezone'], $config['dst']);
+    }
+
+    public function getLocation() {
+        $code = readline('Enter your postal/zip code: ');
+        if ($code && preg_match('/^[0-9A-Za-z]{5,6}$/', $code)) {
+            if ($this->debug) {
+                echo sprintf(PHP_EOL . 'POST %s to %s' . PHP_EOL . PHP_EOL, $code, $this->provider_search_url);
+            }
+            $url = sprintf('%s%s', $this->base_url, $this->provider_search_url);
+            $html = $this->fetchHTML($url, 'POST', array('city_search_string' => $code));
+
+            if ($html) {
+                // get service_area_id options
+                preg_match_all('/<input type="radio" name="service_area_id" value="(.*?)".*?>*<b>(.*?)<\/b>/ms',
+                    $html, $service_area_ids);
+
+                if (sizeof($service_area_ids[0]) < 1) {
+                    echo sprintf(
+                        'Cannot find any service area providers for postal/zip code: %s' . PHP_EOL . PHP_EOL,
+                        $code
+                    );
+                    $this->getLocation();
+                    return;
+                } else {
+                    //var_dump($service_area_ids);
+                    echo 'Enter the number of your city from the list below.' . PHP_EOL . PHP_EOL;
+                    for ($i=0;$i<sizeof($service_area_ids[1]);$i++) {
+                        $code = trim($service_area_ids[1][$i]);
+                        $name = trim($service_area_ids[2][$i]);
+                        $this->service_areas[] = array($code, $name);
+                        echo sprintf("%d\t%s (%s)" . PHP_EOL, $i, $name, $code);
+                    }
+                    $this->postal_code = $code;
+                    $this->getProvider();
+                    return;
+                }
+            } else {
+                throw new \Exception('Error searching for provider. Try again.');
+            }
+        } else {
+            $this->getLocation();
+        }
+    }
+
+    public function getProvider() {
+        $city_selection = readline(PHP_EOL . 'City selection number: ');
+
+        if (isset($this->service_areas[$city_selection])) {
+            if ($this->debug) {
+                echo sprintf(PHP_EOL . 'POST %s, %s to %s' . PHP_EOL . PHP_EOL, $this->postal_code, $this->service_areas[$city_selection][0], $this->provider_search_url);
+            }
+            $url = sprintf('%s%s', $this->base_url, $this->provider_search_url);
+            $html = $this->fetchHTML(
+                $url,
+                'POST',
+                array(
+                    'city_search_string' => $this->postal_code,
+                    'service_area_id' => $this->service_areas[$city_selection][0]
+                )
+            );
+
+            if ($html) {
+                // get all lineup selection options
+                preg_match_all('/<select name="lineup_id" class="textboxedit">(.*?)<\/select>/ms',
+                    $html, $lineup_ids);
+
+                if (sizeof($lineup_ids[0]) < 1) {
+                    echo sprintf(
+                        'Cannot find any providers for City selection number: %s' . PHP_EOL . PHP_EOL,
+                        $city_selection
+                    );
+                    $this->getProvider();
+                    return;
+                } else {
+                    // extract line up data
+                    preg_match_all('/<option value="(.*?)".*?>(.*?)<\/option>/ms',
+                        $lineup_ids[1][0], $lineup_data);
+
+                    echo PHP_EOL . 'Enter the number of your provider from the list below.' . PHP_EOL . PHP_EOL;
+                    for ($i = 0; $i < sizeof($lineup_data[1]); $i++) {
+                        $code = trim($lineup_data[1][$i]);
+                        $name = trim($lineup_data[2][$i]);
+                        $this->provider_data[] = array($code, $name);
+                        echo sprintf("%d\t%s (%s)" . PHP_EOL, $i, $name, $code);
+                    }
+                    $this->city_code = $this->service_areas[$city_selection][0];
+                    $this->getLineup();
+                    return;
+                }
+            } else {
+                throw new \Exception('Error getting provider list. Try again.');
+            }
+        } else {
+            $this->getProvider();
+        }
+    }
+
+    public function getLineup() {
+        $lineup_selection = readline(PHP_EOL . 'Provider selection number: ');
+
+        if (isset($this->provider_data[$lineup_selection])) {
+            $this->initProvider($this->provider_data[$lineup_selection][0], $this->timezone, $this->dst);
+            // TODO: figure out how many pages to get
+            $this->outputSchedule(array(0,20,40,60,80,100,120));
+        } else {
+            $this->getLineup();
+            return;
+        }
     }
 
     public function getSchedule($listing_id, $start_stamp = '', $offset, $timezone, $dst = true) {
@@ -147,7 +270,7 @@ class Tvscraper {
         }
 
         if ($this->display_single) {
-            echo sprintf('Schedule for %s' . PHP_EOL . PHP_EOL, $this->times[1]);
+            echo sprintf(PHP_EOL . 'Schedule for %s' . PHP_EOL . PHP_EOL, $this->times[1]);
         }
 
         foreach (array_unique($lines) as $line) {
@@ -166,8 +289,21 @@ class Tvscraper {
      *
      * @return $html string Response from URL endpoint
      */
-    public function fetchHTML($url) {
-        $html = file_get_contents($url);
-        return $html;
+    public function fetchHTML($url, $method = 'GET', $post_fields = array()) {
+        if ($method == 'GET') {
+            $html = file_get_contents($url);
+            return $html;
+        } elseif ($method == 'POST') {
+            // curl post fields here
+            $ch = curl_init();
+
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post_fields));
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $html = curl_exec($ch);
+            curl_close($ch);
+            return $html;
+        }
     }
 }
